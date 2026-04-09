d108a554b2d9012cb500fc23a251084de716f559
palgato <se.navarrete@hotmail.com>
2026-04-08 23:35:21 -0400

    Add configurable keybind category for CmdPalette controls

--- src/client/java/me/palgato/cmdpalette/client/CmdPaletteClient.java
+++ src/client/java/me/palgato/cmdpalette/client/CmdPaletteClient.java
@@ -6,26 +6,33 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.util.Window;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class CmdPaletteClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        KeyBinding.Category cmdPaletteCategory = KeyBinding.Category.create(Identifier.of("cmdpalette", "keybinds"));
+
         KeyBinding openPalette = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.cmdpalette.open_palette",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_P,
-                KeyBinding.Category.MISC
+            cmdPaletteCategory
+        ));
+
+        KeyBinding openPaletteModifier = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.cmdpalette.open_palette_modifier",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_LEFT_CONTROL,
+            cmdPaletteCategory
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openPalette.wasPressed()) {
-                Window window = client.getWindow();
-                boolean ctrlHeld = InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_LEFT_CONTROL)
-                        || InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_RIGHT_CONTROL);
-                if (ctrlHeld && client.currentScreen == null) {
+            boolean modifierSatisfied = openPaletteModifier.isUnbound() || openPaletteModifier.isPressed();
+            if (modifierSatisfied && client.currentScreen == null) {
                     client.setScreen(new CommandPaletteScreen());
                 }
             }
